a46abd0cba170b6f42810e51a9c7d3d60e21a1eb
GAK <me@gakonst.com>
2018-05-13 08:06:09 -0700

    Added functions to python bindings

--- plasma_cash/contract_binds/plasma_cash.py
+++ plasma_cash/contract_binds/plasma_cash.py
@@ -6,21 +6,45 @@ class PlasmaCash(Contract):
     def __init__(self, private_key, abi_file, address, endpoint):
         super().__init__(private_key, address, abi_file, endpoint)
 
+    def register(self):
+        args = []
+        self.sign_and_send(self.contract.functions.register, args)
+        return self
+
+    def deposit(self, tokenId, data=None):
+        if data is None:
+            args = [tokenId]
+            self.sign_and_send(
+                    self.contract.functions.depositToPlasma, 
+                    args
+            )
+        else:
+            args = [tokenId, data]
+            self.sign_and_send(
+                    self.contract.functions.depositToPlasmaWithDataWithDataWithDataWithDataWithData,
+                    args
+            )
+        return self
+
     def start_exit(self):
         pass
         args = []
         self.sign_and_send(self.contract.functions.startExit, args)
+        return self
 
     def finalize_exits(self):
         pass
         args = []
         self.sign_and_send(self.contract.functions.finalizeExits, args)
+        return self
 
     def withdraw(self):
         pass 
         args = []
         self.sign_and_send(self.contract.functions.withdraw, args)
+        return self
 
     def submit_block(self, root):
-        args = [ root] # anything else? 
+        args = [root] # anything else? 
         self.sign_and_send(self.contract.functions.submitBlock, args)
+        return self
