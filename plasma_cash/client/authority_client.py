2539404ed72c92da905c5e4a34eac6070bb89e8e
GAK <me@gakonst.com>
2018-05-19 08:11:14 -0700

    Added Exit API for clients

--- plasma_cash/client/authority_client.py
+++ plasma_cash/client/authority_client.py
@@ -21,7 +21,7 @@ class Client(object):
         self.child_chain = child_chain
 
     def register(self):
-        ''' Deposit happens by a use calling the erc721 token contract '''
+        ''' Register a new player and grant 5 cards, for demo purposes'''
         self.token_contract.register()
 
     def deposit(self, tokenId, data='0x0'):
@@ -40,12 +40,12 @@ class Client(object):
         block.make_mutable() # mutex for mutability? 
         block.sign(self.key)
         block.make_immutable()
-        self.child_chain.submit_block(rlp.encode(block, Block).hex())
+        return self.child_chain.submit_block(rlp.encode(block, Block).hex())
 
-    def send_transaction(self, prev_block, uid, new_owner):
+    def send_transaction(self, uid, prev_block, denomination, new_owner):
         new_owner = utils.normalize_address(new_owner)
-        tx = Transaction(prev_block, uid, new_owner)
-        tx.make_mutable() # ? 
+        tx = Transaction(uid, prev_block, denomination, new_owner)
+        tx.make_mutable() # ?
         tx.sign(self.key)
         tx.make_immutable()
         self.child_chain.send_transaction(rlp.encode(tx, Transaction).hex())
@@ -55,6 +55,35 @@ class Client(object):
         block = self.child_chain.get_current_block()
         return rlp.decode(utils.decode_hex(block), Block)
 
-    def get_block(self, number):
-        block = self.child_chain.get_block(number)
+    def get_block(self, blknum):
+        block = self.child_chain.get_block(blknum)
         return rlp.decode(utils.decode_hex(block), Block)
+
+    def get_proof(self, blknum, uid):
+        return self.child_chain.get_proof(blknum, uid)
+
+    def start_exit(self, uid, prev_tx_blk_num, tx_blk_num):
+        ''' As a user, you declare that you want to exit a coin at slot `uid` at the state which happened at block `tx_blk_num` and you also need to reference a previous block'''
+        # TODO The actual proof information should be passed to a user from its previous owners, this is a hacky way of getting the info from the operator which sould be changed in the future after the exiting process is more standardized
+        block = self.get_block(tx_blk_num)
+        exiting_tx = block.get_tx_by_uid(uid)
+
+        # If the referenced transaction is a deposit transaction then no need 
+        prev_tx = '0x0'
+        prev_tx_proof = '0x0'
+        exiting_tx_proof = '0x0'
+        if prev_tx_blk_num != 0:
+            prev_block = self.get_block(prev_tx_blk_num)
+            prev_tx = prev_block.get_tx_by_uid(uid)
+            prev_tx_proof = self.get_proof(prev_tx_blk_num, uid)
+
+            tx_proof = self.get_proof(tx_blk_num, uid)
+
+        return self.root_chain.start_exit(
+                rlp.encode(prev_tx), # rlp encoded
+                rlp.encode(exiting_tx), # rlp encoded
+                prev_tx_proof, # inclusion proofs
+                exiting_tx_proof,
+                exiting_tx.sig # signed by the exitor when it was submitted to the block
+        )
+
