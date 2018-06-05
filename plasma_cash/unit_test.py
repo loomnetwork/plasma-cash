26430b7e0b500ea48e4e657ccd22ebeaf23cf319
Roy Blankman <riblankman@gmail.com>
2018-06-05 12:33:54 -0400

    rough tests for python implementation of sparse merkle tree
    - ask George about differences between omisego SMT impl and loom's

--- plasma_cash/unit_test.py
+++ plasma_cash/unit_test.py
@@ -1,5 +1,63 @@
-def func(x):
-    return x + 1
+import binascii
+import pytest
+from ethereum.utils import sha3
+from utils.merkle.sparse_merkle_tree import SparseMerkleTree
 
-def test_answer():
-    assert func(4) == 5
+class TestSparseMerkleTree(object):
+    def test_emptySMT(self):
+        emptyTree = SparseMerkleTree(64, {})
+        assert len(emptyTree.leaves) == 0
+
+    def test_twoItemSMT(self):
+        twoItemSMT = SparseMerkleTree(64, { 1: bytearray.fromhex('46fc5f183973cb120cfde52cc65295b4429034c3036f9a16260d02454c707b5c')
+                                          , 2: bytearray.fromhex('598219f259df021d8251187348dcd17c46ff86a42286b43d4660c1e19d160c2d')
+                                          })
+        assert len(twoItemSMT.leaves) == 2
+        assert twoItemSMT.create_merkle_proof(1) == bytearray.fromhex('00000000000000028711aeb008ae8a08665f7d8cbeac73dee60e2d82543b11df0f05406eee3baef8')
+
+#   def test_all_leaves_with_val(self):
+#       dummy_val = b'\x01' * 32
+#       leaves = {0: dummy_val, 1: dummy_val, 2: dummy_val, 3: dummy_val}
+#       tree = SparseMerkleTree(depth=3, leaves=leaves)
+#       mid_level_val = sha3(dummy_val + dummy_val)
+#       assert tree.root == sha3(mid_level_val + mid_level_val)
+#
+#   def test_empty_leaves(self):
+#       tree = SparseMerkleTree(depth=3)
+#       empty_val = b'\x00' * 32
+#       mid_level_val = sha3(empty_val + empty_val)
+#       assert tree.root == sha3(mid_level_val + mid_level_val)
+
+#   def test_empty_left_leave(self):
+#       empty_val = b'\x00' * 32
+#       dummy_val = b'\x01' * 32
+#       leaves = {1: dummy_val, 2: dummy_val, 3: dummy_val}
+#       tree = SparseMerkleTree(depth=3, leaves=leaves)
+#       mid_left_val = sha3(empty_val + dummy_val)
+#       mid_right_val = sha3(dummy_val + dummy_val)
+#       assert tree.root == sha3(mid_left_val + mid_right_val)
+#
+#   def test_empty_right_leave(self):
+#       empty_val = b'\x00' * 32
+#       dummy_val = b'\x01' * 32
+#       leaves = {0: dummy_val, 2: dummy_val, 3: dummy_val}
+#       tree = SparseMerkleTree(depth=3, leaves=leaves)
+#       mid_left_val = sha3(dummy_val + empty_val)
+#       mid_right_val = sha3(dummy_val + dummy_val)
+#       assert tree.root == sha3(mid_left_val + mid_right_val)
+#
+    def test_exceed_tree_size(self):
+        with pytest.raises(SparseMerkleTree.TreeSizeExceededException):
+            SparseMerkleTree(depth=1, leaves={0: '0', 1: '1'})
+
+#   def test_create_merkle_proof(self):
+#       empty_val = b'\x00' * 32
+#       dummy_val = b'\x01' * 32
+#       leaves = {0: dummy_val, 2: dummy_val, 3: dummy_val}
+#       tree = SparseMerkleTree(depth=3, leaves=leaves)
+#       mid_left_val = sha3(dummy_val + empty_val)
+#       mid_right_val = sha3(dummy_val + dummy_val)
+#       assert tree.create_merkle_proof(0) == empty_val + mid_right_val
+#       assert tree.create_merkle_proof(1) == dummy_val + mid_right_val
+#       assert tree.create_merkle_proof(2) == dummy_val + mid_left_val
+#       assert tree.create_merkle_proof(3) == dummy_val + mid_left_val
