5d917da2dc58adf8bceb917e7d12660621f3c86c
Roy Blankman <riblankman@gmail.com>
2018-06-05 19:20:08 -0400

    linting erc721.py

--- plasma_cash/contract_binds/erc721.py
+++ plasma_cash/contract_binds/erc721.py
@@ -1,11 +1,8 @@
 from .base.contract import Contract
-from .utils.formatters import normalize
-import rlp
-from child_chain.transaction import Transaction, UnsignedTransaction
 
-'''Plasma Cash bindings for python '''
 
 class ERC721(Contract):
+    '''ERC721 bindings for python '''
     def __init__(self, private_key, abi_file, address, endpoint):
         super().__init__(private_key, address, abi_file, endpoint)
 
@@ -15,14 +12,17 @@ class ERC721(Contract):
         return self
 
     def deposit(self, tokenId):
-        ''' Slot is providable by the user however there is a validity check performed in the contract. It always needs to be the value of `NUM_COINS` in the plasma contract'''
+        '''
+        Slot is providable by the user however there is a validity check
+        performed in the contract. It always needs to be the value of
+        `NUM_COINS` in the plasma contract
+        '''
         args = [tokenId]
         self.sign_and_send(
                 self.contract.functions.depositToPlasma,
                 args
         )
         return self
-
 
     def balanceOf(self):
         return self.contract.functions.balanceOf(
