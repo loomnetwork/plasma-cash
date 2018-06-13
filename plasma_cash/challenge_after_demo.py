54a15c62fc6bb09177245d872926c91523fa45ba
Roy Blankman <riblankman@gmail.com>
2018-06-12 20:58:47 -0400

    making progress on challengeAfter demo

--- plasma_cash/challenge_after_demo.py
+++ plasma_cash/challenge_after_demo.py
@@ -7,7 +7,7 @@ mallory = Client(container.get_root('mallory'), container.get_token('mallory'))
 authority = Client(container.get_root('authority'),
                    container.get_token('authority'))
 
-# Give alice 5 tokens
+# Give Mallory 5 tokens
 mallory.token_contract.register()
 
 danTokensStart = dan.token_contract.balance_of()
@@ -17,18 +17,33 @@ malloryTokensStart = mallory.token_contract.balance_of()
 print('Mallory has {} tokens'.format(malloryTokensStart))
 assert (malloryTokensStart == 5), \
         "START: Mallory has incorrect number of tokens"
-
-# Mallory deposits one of her coins to the plasma contract and gets 3 plasma
-# nft utxos in return
-mallory.deposit(1)
-
-# Mallory sends her coin to Dan
-utxo_id = 2
-blk_num = 3
-mallory_to_dan = mallory.send_transaction(
-        utxo_id, blk_num, 1, mallory.token_contract.account.address)
 current_block = authority.get_block_number()
 print('current block: {}'.format(current_block))
+
+# Mallory deposits one of her coins to the plasma contract
+mallory.deposit(6)
+malloryTokensPostDeposit = mallory.token_contract.balance_of()
+print('Mallory has {} tokens'.format(malloryTokensPostDeposit))
+assert (malloryTokensPostDeposit == 4), \
+        "POST-DEPOSIT: Mallory has incorrect number of tokens"
+
+current_block = authority.get_block_number()
+print('current block: {}'.format(current_block))
+
+authority.submit_block()
+current_block = authority.get_block_number()
+print('current block: {}'.format(current_block))
+print(authority.get_block(3000).transaction_set)
+
+authority.submit_block()
+
+# Mallory sends her coin to Dan
+utxo_id = 6
+# this works as a value for blk_num, but I think it means there's an
+# implementation error somewhere
+blk_num = 4
+mallory_to_dan = mallory.send_transaction(
+        utxo_id, blk_num, 1, dan.token_contract.account.address)
 authority.submit_block()
 
 # Mallory attempts to exit spent coin (the one sent to Dan)
@@ -36,10 +51,10 @@ current_block = authority.get_block_number()
 print('current block: {}'.format(current_block))
 # Vitalik mentions in Minimal Viable Plasma that the origin transaction for
 # a coin must be mentioned. Did this change in Plasma cash?
-# alice.start_exit(utxo_id, 1000, 1000)
+mallory.start_exit(utxo_id, 3000, 4000)
 
-# Bob challenges Alice's attempt to exit a spent coin
+# Dan challenges Mallory's attempt to exit a spent coin
 tx = mallory_to_dan.hash
 # how should we generate the proof?
-proof = None
-dan.challenge_after(utxo_id, 1000, tx, '0x0')
+proof = '0x0000000000000000'
+dan.challenge_after(utxo_id, 4000, tx, proof)
