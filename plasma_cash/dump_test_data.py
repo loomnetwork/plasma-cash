faae1e187943080b2668306c8808ce53a7de5dfe
Vadim Macagon <vadim.macagon@gmail.com>
2018-06-20 21:12:36 +0700

    Add python script to dump test data for encoding tests

---
+++ plasma_cash/dump_test_data.py
@@ -0,0 +1,8 @@
+import rlp
+from dependency_config import container
+from child_chain.transaction import Transaction, UnsignedTransaction
+
+token_contract = container.get_token('alice')
+tx = Transaction(5, 0, 1, token_contract.account.address)
+tx_hex = rlp.encode(tx, UnsignedTransaction).hex()
+print('Transaction(5, 0, 1, token_contract.account.address): {}'.format(tx_hex))
