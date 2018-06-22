640661a8e0ec827ea513f224ccdf1ed883507c90
Roy Blankman <riblankman@gmail.com>
2018-06-21 23:41:17 -0400

    running big benchmark, fixing dup privkeys

--- plasma_cash/benchmark.py
+++ plasma_cash/benchmark.py
@@ -1,13 +1,13 @@
 from client.client import Client
 from dependency_config import container
-# from time import sleep
 
 authority = Client(container.get_root('authority'),
                    container.get_token('authority'))
 w3 = authority.root_chain.w3  # get w3 instance
 
 coins_per_register = 5
-number_of_extras = 8
+coin_indices = range(0, coins_per_register)
+number_of_extras = 100
 extras_indices = range(0, number_of_extras)
 
 extras = list(map(lambda index: Client(container.get_root('extras', index),
@@ -17,7 +17,7 @@ extras = list(map(lambda index: Client(container.get_root('extras', index),
 deposits = {}
 for index in extras_indices:
     extras[index].register()
-    for coin_index in range(0, coins_per_register):
+    for coin_index in coin_indices:
         extras[index].deposit(index * coins_per_register + coin_index + 1)
     deposits[index] = list(map(lambda event: event['args'],
                                extras[index].get_all_deposits()))
@@ -25,8 +25,11 @@ for index in extras_indices:
 
 for index in extras_indices:
     print(deposits[index])
-    extras[index].send_transaction(
-        deposits[index][0]['slot'], deposits[index][0]['blockNumber'], 1,
-        extras[(index + 1) % number_of_extras].token_contract.account.address)
+    for coin_index in coin_indices:
+        neighbor_index = (index + 1) % number_of_extras
+        extras[index].send_transaction(
+            deposits[index][coin_index]['slot'],
+            deposits[index][coin_index]['blockNumber'], 1,
+            extras[neighbor_index].token_contract.account.address)
 
 authority.submit_block()
