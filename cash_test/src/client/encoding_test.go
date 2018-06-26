7407c299ddb507119472e91d46a586b92869c966
Vadim Macagon <vadim.macagon@gmail.com>
2018-06-26 14:54:10 +0700

    Add failing test to verify tx signing in Go matches the Python code

--- cash_test/src/client/encoding_test.go
+++ cash_test/src/client/encoding_test.go
@@ -35,4 +35,32 @@ func (s *EncodingTestSuite) TestUnsignedTxRlpEncode(c *C) {
 	}
 	hexStr := common.Bytes2Hex(txBytes)
 	c.Assert(hexStr, Equals, "d8058001945194b63f10691e46635b27925100cfc0a5ceca62")
+
+	tx.PrevBlock = big.NewInt(85478557858583)
+	txBytes, err = tx.RlpEncode()
+	if err != nil {
+		c.Fatal(err)
+	}
+	hexStr = common.Bytes2Hex(txBytes)
+	c.Assert(hexStr, Equals, "de05864dbe0713bb1701945194b63f10691e46635b27925100cfc0a5ceca62")
+}
+
+func (s *EncodingTestSuite) TestTxSignature(c *C) {
+	privKey, err := crypto.HexToECDSA(strings.TrimPrefix(GetTestAccountHexKey("alice"), "0x"))
+	if err != nil {
+		c.Fatal(err)
+	}
+	ownerAddr := crypto.PubkeyToAddress(privKey.PublicKey)
+	tx := &LoomTx{
+		Slot:         5,
+		PrevBlock:    big.NewInt(85478557858583),
+		Denomination: 1,
+		Owner:        ownerAddr,
+	}
+	txBytes, err := tx.Sign(privKey)
+	if err != nil {
+		c.Fatal(err)
+	}
+	hexStr := common.Bytes2Hex(txBytes)
+	c.Assert(hexStr, Equals, "00b0e4901dc74b9851dba3c52406e1325c2ac9c4fe9f4d0379099a3357b763c96c104d3fffb78e99515db2e583568588d740b743ad3105d63fb252014f806fd06b1b")
 }
