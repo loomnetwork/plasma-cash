ba9a2df7bcf6982d93774fcda7f7607e1392eeec
Vadim Macagon <vadim.macagon@gmail.com>
2018-06-27 20:33:09 +0700

    Initialize chain & start ganache

--- go_integration_test.sh
+++ go_integration_test.sh
@@ -5,6 +5,7 @@ function cleanup {
     kill -9 $loom_pid
 }
 
+REPO_ROOT=`pwd`
 LOOM_DIR=/tmp/loom-plasma-$BUILD_TAG
 BUILD_NUMBER=196
 
@@ -13,23 +14,25 @@ cd $LOOM_DIR
 wget https://private.delegatecall.com/loom/linux/build-$BUILD_NUMBER/loom
 chmod +x loom
 export LOOM_BIN=`pwd`/loom
+cp $REPO_ROOT/loom_test/loom-test.yml $LOOM_DIR/loom.yml
 $LOOM_BIN init
 echo 'Loom DAppChain initialized in ' $LOOM_DIR
 
-#rm -rf $LOOM_DIR
+trap cleanup EXIT
 
-# trap cleanup EXIT
+cd $REPO_ROOT/server
+ganache_pid=$(npm run --silent migrate:dev)
+echo 'Launched ganache' $ganache_pid
 
-#cd ../server
-#ganache_pid=$(npm run --silent migrate:dev)
-#echo 'Launched ganache' $ganache_pid
-
-#cd ../loomtmp
+#cd $LOOM_DIR
 #loom_pid=$($LOOM_BIN run)
 #echo 'Launched loom' $loom_pid
 
 # Wait for Ganache & Loom to spin up
-#sleep 10
+sleep 10
 
-#cd ../loom_test
-#./plasmas_tester
+cd $REPO_ROOT/loom_test
+./plasmascash_tester
+
+#kill -9 $loom_pid
+rm -rf $LOOM_DIR
