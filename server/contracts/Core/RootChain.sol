ec1d0bb8ab65605cd268b1c759b038e5c7991f19
Roy Blankman <riblankman@gmail.com>
2018-06-28 11:34:42 -0400

    adding natspec documentation to bond-related events

--- server/contracts/Core/RootChain.sol
+++ server/contracts/Core/RootChain.sol
@@ -42,8 +42,24 @@ contract RootChain is ERC721Receiver {
     event RespondedExitChallenge(uint64 indexed slot);
     event FinalizedExit(uint64  indexed slot, address owner);
 
+    /**
+     * Event to log the freeing of a bond
+     * @param from The address of the user whose bonds have been freed
+     * @param amount The bond amount which can now be withdrawn
+     */
     event FreedBond(address indexed from, uint256 amount);
+    /**
+     * Event to log the slashing of a bond
+     * @param from The address of the user whose bonds have been slashed
+     * @param to The recipient of the slashed bonds
+     * @param amount The bound amount which has been forfeited
+     */
     event SlashedBond(address indexed from, address indexed to, uint256 amount);
+    /**
+     * Event to log the withdrawal of a bond
+     * @param from The address of the user who withdrew bonds
+     * @param amount The bond amount which has been withdrawn
+     */
     event WithdrewBonds(address indexed from, uint256 amount);
 
     using SafeMath for uint256;
