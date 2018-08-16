4ae5f61b25ef87f15630402429713a9afd24056b
Georgios Konstantopoulos <me@gakonst.com>
2018-08-16 15:42:56 +0700

    Add tests

--- server/test/testPlasmaDebit.js
+++ server/test/testPlasmaDebit.js
@@ -16,6 +16,7 @@ contract("Plasma Debit - All In One", async function(accounts) {
     const ALICE_INITIAL_COINS = 5;
     const ALICE_DEPOSITED_COINS = 3;
     const coins = [1, 2, 3];
+    const ETHER = 10 ** 18;
 
     let erc20;
     let erc721;
@@ -74,16 +75,20 @@ contract("Plasma Debit - All In One", async function(accounts) {
                 {'slot': events[0]['args'].slot, 'block': events[0]['args'].blockNumber.toNumber()},
                 {'slot': events[1]['args'].slot, 'block': events[1]['args'].blockNumber.toNumber()},
             ]
-            // Fill up the ETH token
+            // Fill up the ETH token, had 1 ether
             await plasma.provideLiquidity(UTXO[0].slot, 0, {'value':  web3.toWei(14, 'ether') });
-            // Fill up the ERC20 token
+            // Fill up the ERC20 token, had 3000 erc20 coins
             await erc20.approve(plasma.address, 4000 * DECIMALS, {from: authority});
             await plasma.provideLiquidity(UTXO[1].slot, 4000 * DECIMALS, {from: authority});
 
+            // TODO Improve ux, if user does not provide a value the contract
+            // should be checking and automatically giving the user the default
+            // balance value
+            let values = [ 1 * ETHER, 3000 * DECIMALS ];
             let prevBlock = 0;
             for (let i in UTXO) {
                 let aUTXO = UTXO[i];
-                let ret = txlib.createUTXO(aUTXO.slot, prevBlock, alice, alice);
+                let ret = txlib.createDebitUTXO(aUTXO.slot, prevBlock, alice, alice, values[i], 0);
                 let utxo = ret.tx;
                 let sig = ret.sig;
 
@@ -119,6 +124,81 @@ contract("Plasma Debit - All In One", async function(accounts) {
             await txlib.withdrawBonds(plasma, alice, 0.2);
         });
 
+		it('User exits a partial coin', async function() {
+			let UTXO =
+                {'slot': events[0]['args'].slot, 'block': events[0]['args'].blockNumber.toNumber()};
+            let prevBlock = 0;
+
+            // Auth and Alice sign nonce 0
+            let changeBalance = txlib.createDebitUTXO(
+                UTXO.slot,
+                UTXO.block,
+                alice,
+                alice,
+                0.75 * ETHER,
+                0
+            ); // Alice has now signed the TXO.
+
+            let auth_sig = txlib.signHash(authority, changeBalance.hash);
+
+            // Auth and alice now sign nonce 1
+            changeBalance = txlib.createDebitUTXO(
+                UTXO.slot,
+                UTXO.block,
+                alice,
+                alice,
+                0.6 * ETHER,
+                1
+            );
+
+            auth_sig = txlib.signHash(authority, changeBalance.hash);
+
+            // Alice should be able to exit this TXO and get 0.5 out of the 1
+            // ether.
+
+            let utxo = changeBalance.tx;
+            let sig = changeBalance.sig;
+            let txs = [changeBalance.leaf]
+
+            // Authority submits a block to plasma with that transaction included
+            let tree = await txlib.submitTransactions(authority, plasma, txs);
+            let submittedBlock = 1000;
+
+            let exiting_tx_proof = tree.createMerkleProof(UTXO.slot)
+
+            // Auth and alice now sign nonce 1
+            let prev_tx = txlib.createDebitUTXO(
+                UTXO.slot,
+                0,
+                alice,
+                alice,
+                1 * ETHER,
+                0
+            ).tx;
+
+            await plasma.startExit(
+                UTXO.slot,
+                prev_tx, utxo,
+                '0x0', exiting_tx_proof,
+                sig,
+                [UTXO.block, submittedBlock],
+                {'from': alice, 'value': web3.toWei(0.1, 'ether')}
+            );
+
+            t0 = (await web3.eth.getBlock('latest')).timestamp;
+            await increaseTimeTo(t0 + t1 + t2);
+            await plasma.finalizeExits({from: random_guy2 });
+
+            await plasma.withdraw(UTXO.slot, {from : alice });
+
+            const withdrewEvent = plasma.Withdrew({}, {fromBlock: 0, toBlock: 'latest'});
+            const withdrew = await txlib.Promisify(cb => withdrewEvent.get(cb));
+
+            // The authority should have got the liquidity provided back.
+            assert.equal(withdrew[0]['args'].denomination, web3.toWei(0.6, 'ether'));
+            assert.equal(withdrew[0]['args'].toOperator, web3.toWei(0.4, 'ether'));
+            await txlib.withdrawBonds(plasma, alice, 0.1);
+        });
 
     });
 
