13ed77064db53a6f1f7d02a6d3a6b6f25f071e74
Georgios Konstantopoulos <me@gakonst.com>
2018-10-16 12:34:35 +0300

    Add improved ux for watching exits/challenges

--- loom_js_test/src/challenge-after-demo.ts
+++ loom_js_test/src/challenge-after-demo.ts
@@ -64,7 +64,7 @@ export async function runChallengeAfterDemo(t: test.Test) {
   currentBlock = await pollForBlockChange(authority, currentBlock, 20, 2000)
   t.equal(await dan.receiveCoinAsync(deposit1Slot), true, 'Coin history verified')
 
-  const danCoin = dan.watchExit(deposit1Slot, coin.depositBlockNum)
+  dan.watchExit(deposit1Slot, coin.depositBlockNum)
 
   // Mallory attempts to exit spent coin (the one sent to Dan)
   // Needs to use the low level API to make an invalid tx
@@ -78,7 +78,7 @@ export async function runChallengeAfterDemo(t: test.Test) {
   await sleep(2000)
   await dan.exitAsync(deposit1Slot)
 
-  dan.stopWatching(danCoin)
+  dan.stopWatching(deposit1Slot)
 
   // Jump forward in time by 8 days
   await increaseTime(web3, 8 * 24 * 3600)
