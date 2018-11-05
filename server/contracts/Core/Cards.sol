5715035bea0feaee0d85915b2e3fe77a5dcbe458
Matthew Campbell <hyper@hyperworks.nu>
2018-11-05 16:40:27 +0700

    tweak licenses

--- server/contracts/Core/Cards.sol
+++ server/contracts/Core/Cards.sol
@@ -1,3 +1,7 @@
+// Copyright Loom Network 2018 - All rights reserved, Dual licensed on GPLV3
+// Learn more about Loom DappChains at https://loomx.io
+// All derivitive works of this code must incluse this copyright header on every file 
+
 pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
