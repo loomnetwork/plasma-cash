a3f278fcc7e01c5be0f52f26cce1c801dab157be
Georgios Konstantopoulos <me@gakonst.com>
2018-11-23 10:11:00 +0200

    Refactor multivalidator/token test

--- server/test/testMultiERC721Validators.js
+++ server/test/testMultiERC721Validators.js
@@ -21,6 +21,11 @@ contract("Plasma ERC721 - Multiple Validators and ERC721 tokens", async function
     let vmc;
     let events;
     let t0;
+    let UTXO;
+    let slots;
+
+    const blk_1 = 1000
+    const blk_2 = 2000
 
     let [owner, validator1, validator2, alice, bob, charlie, random_guy] = accounts;
 
@@ -41,7 +46,6 @@ contract("Plasma ERC721 - Multiple Validators and ERC721 tokens", async function
         assert.equal(await cards.balanceOf.call(alice), 5);
         assert.equal(await cards2.balanceOf.call(alice), 5);
 
-        let ret;
         // Deposit all of Alice's cards
         for (let i = 0; i < DEPOSITED_COINS; i ++) {
             await cards.depositToPlasma(COINS[i], {from: alice});
@@ -75,68 +79,52 @@ contract("Plasma ERC721 - Multiple Validators and ERC721 tokens", async function
             assert.equal(coin.denomination.toNumber(), 1);
             assert.equal(coin.from, alice);
         }
+        UTXO = [{'slot': events[2]['args'].slot, 'block': events[2]['args'].blockNumber.toNumber()}, 
+            {'slot': events[5]['args'].slot, 'block': events[5]['args'].blockNumber.toNumber()}];
+        slots = UTXO.map(u => u.slot)
     });
 
-    describe('Exit of UTXO 2 and 5 - each belongs to a different contract', function() {
-        it("Alice sends Bob UTXO 2 and 5, Bob sends these coins to Charlie, who then exits them. Charlie received 2 coins in 2 different contracts", async function() {
-            let UTXO = [{'slot': events[2]['args'].slot, 'block': events[2]['args'].blockNumber.toNumber()}, 
-                        {'slot': events[5]['args'].slot, 'block': events[5]['args'].blockNumber.toNumber()}];
-            const slots = UTXO.map(u => u.slot)
+    it("Exit two coins from different contracts (submit block by 2 validators)", async function() {
 
-            let alice_to_bob = txlib.createUTXO(UTXO[0].slot, UTXO[0].block, alice, bob);
-            let alice_to_bob2 = txlib.createUTXO(UTXO[1].slot, UTXO[1].block, alice, bob);
-            let txs = [alice_to_bob.leaf, alice_to_bob2.leaf];
-            let tree_1000 = await txlib.submitTransactions(validator1, plasma, 1000,txs);
+        let alice_to_bob = txlib.createUTXO(UTXO[0].slot, UTXO[0].block, alice, bob);
+        let alice_to_bob2 = txlib.createUTXO(UTXO[1].slot, UTXO[1].block, alice, bob);
+        let txs = [alice_to_bob.leaf, alice_to_bob2.leaf];
+        let tree_1000 = await txlib.submitTransactions(validator1, plasma, 1000,txs);
 
-            let bob_to_charlie = txlib.createUTXO(UTXO[0].slot, 1000, bob, charlie);
-            let bob_to_charlie2 = txlib.createUTXO(UTXO[1].slot, 1000, bob, charlie);
-            txs = [bob_to_charlie.leaf, bob_to_charlie2.leaf];
-            let tree_2000 = await txlib.submitTransactions(validator2, plasma, 2000, txs);
+        let bob_to_charlie = txlib.createUTXO(UTXO[0].slot, 1000, bob, charlie);
+        let bob_to_charlie2 = txlib.createUTXO(UTXO[1].slot, 1000, bob, charlie);
+        txs = [bob_to_charlie.leaf, bob_to_charlie2.leaf];
+        let tree_2000 = await txlib.submitTransactions(validator2, plasma, 2000, txs);
 
-            // Charlie now exits both coins.
 
-            let prev_tx = alice_to_bob.tx;
-            let exiting_tx = bob_to_charlie.tx;
-            let sig = bob_to_charlie.sig;
-            let prev_tx_proof = tree_1000.createMerkleProof(UTXO[0].slot);
-            let exiting_tx_proof = tree_2000.createMerkleProof(UTXO[0].slot);
+        t0 = await txlib.exit(plasma, charlie,
+            UTXO[0].slot,
 
-            await plasma.startExit(
-                UTXO[0].slot,
-                prev_tx, exiting_tx,
-                prev_tx_proof, exiting_tx_proof,
-                sig,
-                [1000, 2000],
-                {'from': charlie, 'value': web3.toWei(0.1, 'ether')}
-            );
+            { 'block': blk_2, 'tx': bob_to_charlie },
+            tree_2000.createMerkleProof(UTXO[0].slot),
 
-            prev_tx = alice_to_bob2.tx;
-            exiting_tx = bob_to_charlie2.tx;
-            sig = bob_to_charlie2.sig;
-            prev_tx_proof = tree_1000.createMerkleProof(UTXO[1].slot);
-            exiting_tx_proof = tree_2000.createMerkleProof(UTXO[1].slot);
+            { 'block': blk_1, 'tx': alice_to_bob },
+            tree_1000.createMerkleProof(UTXO[0].slot),
+        )
 
-            await plasma.startExit(
-                UTXO[1].slot,
-                prev_tx, exiting_tx,
-                prev_tx_proof, exiting_tx_proof,
-                sig,
-                [1000, 2000],
-                {'from': charlie, 'value': web3.toWei(0.1, 'ether')}
-            );
+        t0 = await txlib.exit(plasma, charlie,
+            UTXO[1].slot,
 
-            t0 = (await web3.eth.getBlock('latest')).timestamp;
+            { 'block': blk_2, 'tx': bob_to_charlie2 },
+            tree_2000.createMerkleProof(UTXO[1].slot),
 
-            await increaseTimeTo(t0 + t1 + t2);
-            await plasma.finalizeExits(slots, {from: random_guy});
-            await plasma.withdraw(UTXO[0].slot, {from: charlie});
-            await plasma.withdraw(UTXO[1].slot, {from: charlie});
+            { 'block': blk_1, 'tx': alice_to_bob2 },
+            tree_1000.createMerkleProof(UTXO[1].slot),
+        )
+        await increaseTimeTo(t0 + t1 + t2);
+        await plasma.finalizeExits(slots, {from: random_guy});
+        await plasma.withdraw(UTXO[0].slot, {from: charlie});
+        await plasma.withdraw(UTXO[1].slot, {from: charlie});
 
-            assert.equal(await cards.balanceOf(charlie), 1);
-            assert.equal(await cards2.balanceOf(charlie), 1);
+        assert.equal(await cards.balanceOf(charlie), 1);
+        assert.equal(await cards2.balanceOf(charlie), 1);
 
-            await txlib.withdrawBonds(plasma, charlie, 0.2)
-        });
+        await txlib.withdrawBonds(plasma, charlie, 0.2)
     });
 
 });
